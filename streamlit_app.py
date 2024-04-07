ebba2e6949f77ccb34eeff4d6789c4e3cccc7542
Sanjana <97579455+kitty57@users.noreply.github.com>
2024-04-07 18:08:27 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ def generate_epipro(name, genre, protagonist):
     prompt = f'''
     "As an experienced and witty writer, write a prologue (it should be something like a backstory of some kind of incident happening to the main characters, explain the incident as a scene in a character's POV, before they actually meet, add descriptions, dialogues, and other such elements, the characters shouldn't meet yet, it should just show a backstory of how their real lives are) and an epilogue (this should be some sort of incident with dialogues and such elements after the main story is over) of 1500 words each that seems as though written by a human, for a novel named {name}, of the genre/genres {genre}, with the protagonists {protagonist}. Add a lot of funny and witty dialogues and remarks whenever possible. Both of that should be from a character's POV only, that is the number 1 rule!!
     The output should be of the following form :
-    =============================== Novel name -- and a catchphrase ===============================
+    Novel name -- and a catchphrase
     Prologue:
     ### the prologue goes here!
     -----
@@ -30,7 +30,10 @@ protagonist = st.text_area("Enter Protagonists (Character Names and Descriptions
 if st.button("Generate Prologue & Epilogue"):
     if name and genre and protagonist:
         result = generate_epipro(name, genre, protagonist)
-        st.markdown(result)
+        r1,r2=result.split('Epilogue')
+        st.info(r1)
+        dt.markdown('Epilogue:')
+        st.info(r2)
     else:
         st.error("Please fill in all fields.")
 
